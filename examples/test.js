5400787d251e9bc6fefad99884fd161a10baeba7
John-Lin <linton.tw@gmail.com>
2015-12-01 23:50:52 +0800

    using local lib

--- examples/test.js
+++ examples/test.js
@@ -1,7 +1,7 @@
 'use strict';
-let Awesome = require('awesomelists-index');
+let Awesome = require('../index.js');
 
 // Given a repository name with author ex: vinta/awesome-python
-let py = new Awesome('vinta/awesome-python');
+let py = new Awesome('veggiemonk/awesome-docker');
 
 py.makeIndexJson((err, res) => {console.log(res);});
