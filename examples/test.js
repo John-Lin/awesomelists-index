6085cdcca8fb6e07fb0144c23a4783347a3f6289
lockys <locky4567@gmail.com>
2015-12-03 12:12:58 +0800

    retrieve general

--- examples/test.js
+++ examples/test.js
@@ -1,5 +1,5 @@
 'use strict';
 let Awesome = require('../index.js');
-let a = new Awesome('https://raw.githubusercontent.com/vinta/awesome-python/master/README.md');
+let a = new Awesome('https://raw.githubusercontent.com/cjwirth/awesome-ios-ui/master/README.md');
 
 a.makeIndexJson((err, res) => {console.log(res);});
