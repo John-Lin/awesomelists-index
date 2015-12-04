2e7d191207e4090943d189049eb9fe55b05e1084
Che-Wei Lin <linton.tw@gmail.com>
2015-12-04 23:13:12 +0800

    read/write json file using jsonfile package

--- examples/gen-all-awesome-list.js
+++ examples/gen-all-awesome-list.js
@@ -5,15 +5,13 @@
 'use strict';
 let Awesome = require('../index.js');
 let jsonfile = require('jsonfile');
-let fs = require('fs');
 
-let nameMapArray = JSON.parse(fs.readFileSync('../json/awesome-detailed-info.json', 'utf8'));
+let nameMapArray = jsonfile.readFileSync('../json/awesome-detailed-info.json');
 let opt = {
   // token is optional parameter
   token: process.env.TOKEN || 'GITHUB_TOKE',
 };
 for (let i of nameMapArray) {
-  console.log(i.githubPath);
   opt.repo = i.githubPath;
   if (opt.repo) {
     let a = new Awesome(opt);
